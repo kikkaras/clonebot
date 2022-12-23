3501179e4a7ccbca981fb1fb505af568ad3f7ab7
kikkaras <115210577+kikkaras@users.noreply.github.com>
2022-12-23 16:35:30 +0000

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -21,7 +21,7 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "b6e73b921948a2f437d19bc2c2aaa595")
 
     # Generate a user session string
-    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "AQAjp51Ylxd_5PlQ1o99gxMwdK3hIe71_YA65Ia5qFM-iewGvRxGzqsR1v0oB4hM1PojpM-UQpNdB-NWWssgVVZf2orew0R5X1gRKRIMZxTwFeq91jqd6Z5NeCmgV5S9sJ7MKnyiVDmy7TlU2IG0vwQvBcD5MQAo9Yzezs3gRj5be_5Nk5bKmKG-G3E1H4XBIB4uAixoiN-zQkL6x-2Jvvmdp1EBxPzBHFJxakNl_1gwMwz6VWtrre_gfruRld1RXducshgrydpYqj2A09nqcaZGKEpqvlCvf1TI-NYcPMg6X3qfTstPi7RkDRQUduXfrmh9Jt0hQMXm9cTssmOAYZYZAAAAATvpR2AA")
+    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "AQARqLeO2MscpiEIRoMiIXcZ4LT1yLXeVYNqJP_O5yivvGpyCgqDogYOAnlMEt001ETuCTQ0ZEURHnnxF57TmIfGvrvohd9ODyYU7dyGdjoH40FOC_T8sMS_EL6VaADC3jVfNZ0tNiXURqWWmeRIsEmnDXUxABZ4X53sA51YgXU5RIVqpiUjnndgRFAyyvyvdAa1W5-SGOztd9zJ7pPT3e3idMfmpnbFkcLMkLv3vieGZjPdAAYgbL9pcdBY50A1hiAi8QiFuiPgpXFp53SjEYrKTocMNXEWYWD3qDaJCrc5u4tuB7x_GvcI0CdsYH3kBP_aRVXNjKjjasKxt9V9mVX_AAAAATvpR2AA")
 
     # Database URI
     DB_URI = os.environ.get("DATABASE_URL", "postgresql://dipon:amithnajanaja333@localhost:5432/cluster100")
