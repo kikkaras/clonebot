02260d18b06a951c6653f0e781c3bd484afdbfa7
kikkaras <115210577+kikkaras@users.noreply.github.com>
2022-12-23 16:56:37 +0000

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -10,11 +10,11 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 class Config(object):
 
-    TG_BOT_TOKEN = ""
-    APP_ID = 
-    API_HASH = ""
-    TG_USER_SESSION = ""
-    DB_URI = ""
+    TG_BOT_TOKEN = "5840686387:AAE3m7iaYYDobonnra9iQqTpGU3qVhTmreA"
+    APP_ID = 25031360
+    API_HASH = "b6e73b921948a2f437d19bc2c2aaa595"
+    TG_USER_SESSION = "AQARqLeO2MscpiEIRoMiIXcZ4LT1yLXeVYNqJP_O5yivvGpyCgqDogYOAnlMEt001ETuCTQ0ZEURHnnxF57TmIfGvrvohd9ODyYU7dyGdjoH40FOC_T8sMS_EL6VaADC3jVfNZ0tNiXURqWWmeRIsEmnDXUxABZ4X53sA51YgXU5RIVqpiUjnndgRFAyyvyvdAa1W5-SGOztd9zJ7pPT3e3idMfmpnbFkcLMkLv3vieGZjPdAAYgbL9pcdBY50A1hiAi8QiFuiPgpXFp53SjEYrKTocMNXEWYWD3qDaJCrc5u4tuB7x_GvcI0CdsYH3kBP_aRVXNjKjjasKxt9V9mVX_AAAAATvpR2AA"
+    DB_URI = "postgresql://dipon:amithnajanaja333@localhost:5432/cluster100"
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
