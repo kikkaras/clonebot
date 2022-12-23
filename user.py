95ec8c12c2198322a045bcc326a6614806aa178b
kikkaras <115210577+kikkaras@users.noreply.github.com>
2022-12-23 17:16:09 +0000

    Update user.py

--- user.py
+++ user.py
@@ -4,8 +4,8 @@ from pyrogram import Client
 from pyrogram.enums import ParseMode
 
 if os.environ.get("ENV", False):
-    from sample_config import Config
-    from sample_config import LOGGER
+    from config import Config
+    from config import LOGGER
 else:
     from config import Config
     from config import LOGGER
